1c5c18144230395d5a84a07104f96e68903485dd
jerry <369932775@qq.com>
2019-03-27 11:27:54 +0800

    添加404页面

--- src/views/errors/error404.vue
+++ src/views/errors/error404.vue
@@ -1,9 +1,19 @@
-<style scoped>
-
+<style>
+    .bg404{
+        background-color: rgb(55,162,218) !important;
+    }
+    .img404{
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: -150px;
+        bottom: 0;
+        margin: auto;
+    }
 </style>
 <template>
-    <div>
-        {{ msg }}
+    <div class="bg">
+        <img class="img404" src="/static/errors/404page.png" alt="">
     </div>
 </template>
 <script>
@@ -12,6 +22,12 @@
             return {
                 msg: "找不到当前页面"
             }
+        },
+        beforeCreate: function() {
+            document.getElementsByTagName("body")[0].className="bg404";
+        },
+        beforeDestroy: function() {
+            document.body.removeAttribute("class","bg404");
         },
         methods: {
 
